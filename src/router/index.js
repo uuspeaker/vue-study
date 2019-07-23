b2abb976a16fe16030b9cd46b466b70d8228a17d
unknown <tianhan@citiccard.com>
2019-07-23 17:04:26 +0800

    reset project

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-import First from '@/components/First'
 
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
   routes: [
-    {path: '/',name: 'HelloWorld',component: HelloWorld},
-    {path: '/First',name: 'First',component: First},
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    }
   ]
 })
