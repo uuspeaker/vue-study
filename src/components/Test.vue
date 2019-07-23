640a324c7d8e72962d9ba0faf574ad7bc2fc851b
unknown <tianhan@citiccard.com>
2019-07-23 18:02:37 +0800

    router whitespace warning

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h1>{{ name }}</h1>
+    <h1>{{msg}}</h1>
+    <h1>hello</h1>
   </div>
 </template>
 
@@ -10,8 +10,7 @@ export default {
   name: 'hello',
   data () {
     return {
-      msg: '欢迎来到vue教程！',
-      name: '我是vue'
+      msg: '欢迎来到菜鸟教程！'
     }
   }
 }
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Test from '@/components/Test'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+    {path: '/', name: 'HelloWorld', component: HelloWorld},
+    {path: '/Test', name: 'Test', component: Test}
   ]
 })
