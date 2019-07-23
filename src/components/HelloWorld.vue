dc57e76baa8633fa4aab49365cf2328fab7fd3e0
unknown <tianhan@citiccard.com>
2019-07-23 16:03:41 +0800

    init-project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>原始字符串: {{ message }}</p>
+  <p>计算后反转字符串: {{ reversedMessage }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: '欢迎来到菜鸟教程！',
+      message: 'Runoob!'
+    }
+  }
+}
+</script>
