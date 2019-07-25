1a1e4696800119483d6f8241f0caf0b8dd9e45f5
unknown <tianhan@citiccard.com>
2019-07-25 17:59:14 +0800

    add picture demo

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-import Test from '@/components/Test'
+import Index from '@/page/main/Index'
+import Head from '@/page/main/Head'
+import Pic from '@/page/main/Pic'
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
   routes: [
-    {path: '/', name: 'HelloWorld', component: HelloWorld},
-    {path: '/Test', name: 'Test', component: Test}
+    {path: '/1', name: 'HelloWorld', component: HelloWorld},
+    {path: '/', name: 'Index', component: Index},
+    {path: '/head', name: 'Head', component: Head}
+    {path: '/pic', name: 'Pic', component: Pic}
   ]
 })
